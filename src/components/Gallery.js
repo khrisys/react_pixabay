e2fe25e5def2f94b0006553338fa80bfce6afc8f
Heremiti <heremiti@khris.fr>
2022-03-24 21:31:31 +0100

    Ajout pagination en haut Test de pagination avec composant mui
    Signed-off-by: Heremiti <heremiti@khris.fr>

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,7 @@ import axios              from 'axios';
 class Gallery extends Component {
 
 
+	const;
 
 	/**
 	 * Initialisation des variables utiles à l'ffichage des photos de pixabay. Ici, je mets des valeurs par defaut plutot que rien
@@ -13,15 +14,17 @@ class Gallery extends Component {
 		super(props);
 		this.state = {
 			hits: [],
-			currentPage: 1,
 			api_key: '16870681-fc0731d7d44beb3487658dcec',
-			pixs_per_page: 8,
-			keyword: 'millau'
+			pixs_per_page: 9,
+			keyword: 'paris',
+			currentPage: 1,
+			totalPages: 1,
+			nbPages: []
 		}
 	}
 
 	/**
-	 * UNe fois que le composant est chargé, j'envoie la requete pour recuperer les photos
+	 * Une fois que le composant est chargé, j'envoie la requete pour recuperer les photos.Ceci provoquera donc un comprtement par defaut
 	 */
 	componentDidMount() {
 		this.getHits();
@@ -31,11 +34,14 @@ class Gallery extends Component {
 	 * Recuperation des photos (nommées 'hits' dans pixabay) depuis pixabay (importer la dependance axios pour les requetes apres 'npm i --save axios')
 	 */
 	getHits() {
-		let url = "https://pixabay.com/api/?key=16870681-fc0731d7d44beb3487658dcec&q="+this.state.keyword+"&per_page="+this.state.pixs_per_page;
+		let url = "https://pixabay.com/api/?key=" + this.state.api_key + "&q=" + this.state.keyword + "&per_page=" + this.state.pixs_per_page + "&page=" + this.state.currentPage;
 		console.log("URL DE REQUETE / " + url);
 		axios.get(url).then((resp) => {
+			let totalPages = (resp.data.totalHits % this.state.pixs_per_page === 0) ? resp.data.totalHits / this.state.pixs_per_page : Math.ceil(resp.data.totalHits / this.state.pixs_per_page);
 			this.setState({
-				hits: resp.data.hits
+				hits: resp.data.hits,
+				totalPages: totalPages,
+				nbPages: new Array(totalPages).fill(0)
 			})
 		}).catch((err) => {
 			console.log(err);
@@ -48,6 +54,7 @@ class Gallery extends Component {
 		})
 	};
 
+
 	/**
 	 * TWO WAY BINDING : la valeur de la zone de texte va vers le state > A chaque fois que le state change, il s'affiche dans la zone de texte (Le champ est controlé par React)
 	 *
@@ -55,14 +62,35 @@ class Gallery extends Component {
 	 * sans que le browser ne soit atteint et rafraichisse la page entiere, remettant par defaut le mot clef defini dans le constructeur.
 	 * @param event
 	 */
-	onSubmitResearch = (event) =>{
+	onSubmitResearch = (event) => {
 		event.preventDefault();
 		this.getHits();
 	};
 
+	/**
+	 * Navigation via la pagination
+	 * Dans cette methode,on passe l'appel vers l'api pixabay en 2e param. La methode setState etant async, si on met setState() en dehors de setState, getHits() sera appelé
+	 * avant que 'currentPage' soit mis à jour.
+	 *
+	 *ATTENTION : dans le html, à l'appel de onClickPagination, utiliser une fonction lambda pour appeler onClickPagination()
+	 **/
+	onClickPagination = (index) => {
+		this.setState({
+			currentPage: index
+		}, () => {
+			this.getHits();
+		});
+	};
+
+	handleChange = (event, value) => {
+		this.setState({
+			currentPage: value
+		})
+	};
+
+
 	render() {
-		return (
-			<div>
+		return <div>
 				<form onSubmit={this.onSubmitResearch}>
 					<div className="row m-2 p-2">
 						<div className="col">
@@ -73,10 +101,22 @@ class Gallery extends Component {
 							       value={this.state.keyword}/>
 						</div>
 						<div className="col-auto">
-							<button className="btn btn-primary" type="submit">Chercher</button>
+							<button className="btn btn-primary"
+							        type="submit">Chercher</button>
 						</div>
 					</div>
 				</form>
+
+			<div>
+					<ul className="nav nav-pills">
+						{this.state.nbPages.map((page, index) =>
+							<button key={index + 1}
+							        className={this.state.currentPage === index + 1 ? 'btn btn-primary' : 'btn'}
+								/*ONCLICK PREND ICI UNE FONCTION ANONYME EN PARAM, QUI APPELLE ALORS ONCLICKPAGINATION()*/
+								    onClick={() => this.onClickPagination(index + 1)}>{index + 1}</button>
+						)}
+					</ul>
+				</div>
 				<div className="row">
 					{
 						this.state.hits.map((hit, index) =>
@@ -84,17 +124,41 @@ class Gallery extends Component {
 							     key={index}>
 								<div className="card">
 									<div className="card-header">{hit.tags} | <strong>Resolution : </strong>{hit.webformatWidth} x {hit.webformatHeight}</div>
-									<div className="card-body"><img className="card-img"
-									                                height={300}
+									<div className="card-body"><img alt="Vignette manquante"
+									                                className="card-img"
+									                                height={200}
 									                                src={hit.webformatURL}
-									                                alt="Photo manquante"/></div>
+									/></div>
 								</div>
 							</div>
 						)
 					}
 				</div>
-			</div>
-		);
+
+
+			{/*<Pagination count={this.state.totalPages}
+			 showFirstButton
+			 showLastButton
+			 variant="outlined"
+			 color="primary"
+			 size="small"
+			 onChange={this.handleChange}
+			 page={this.state.currentPage}
+			 defaultPage={4}
+			 />*/}
+
+
+			<div>
+			 <ul className="nav nav-pills m-1">
+			 {this.state.nbPages.map((page, index) =>
+				 <button key={index + 1}
+				         className={this.state.currentPage === index + 1 ? 'btn btn-primary' : 'btn btn-link'}
+					 /*ONCLICK PREND ICI UNE FONCTION ANONYME EN PARAM, QUI APPELLE ALORS ONCLICKPAGINATION()*/
+					     onClick={() => this.onClickPagination(index + 1)}>{index + 1}</button>
+			 )}
+			 </ul>
+			 </div>
+			</div>;
 	}
 }
 
