54b84869d1adbfdc2bc845b9ae379cfd0c93bb12
Heremiti <heremiti@khris.fr>
2022-04-08 08:49:02 +0200

    Config de deployement pour heroku
    Signed-off-by: Heremiti <heremiti@khris.fr>

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
-//server.jsconst express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
-	return res.send('pong');
-});
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});app.listen(port);
+const path = require('path');
+const port = process.env.PORT || 3001;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => console.log("Listening on Port", port));
